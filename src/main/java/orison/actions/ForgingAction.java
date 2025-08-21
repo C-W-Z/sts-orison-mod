5a3d90391602470db7d738a99040ebed0600b40c
C-W-Z <chenweizhang3021@gmail.com>
2025-08-21 22:46:15 +0800

    update Forging Effect

--- src/main/java/orison/actions/ForgingAction.java
+++ src/main/java/orison/actions/ForgingAction.java
@@ -4,6 +4,7 @@ import com.evacipated.cardcrawl.mod.stslib.actions.common.MultiGroupSelectAction
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup.CardGroupType;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
 
 public class ForgingAction extends MultiGroupSelectAction {
@@ -11,7 +12,9 @@ public class ForgingAction extends MultiGroupSelectAction {
         super("Select " + amount + " card" + (amount > 1 ? "s" : "") + " to Upgrade", (cards, card2Group) -> {
             for (AbstractCard c : cards) {
                 c.upgrade();
-                AbstractDungeon.effectList.add(0, new ShowCardBrieflyEffect(c.makeStatEquivalentCopy()));
+                AbstractCard tmpCard = c.makeStatEquivalentCopy();
+                AbstractDungeon.effectList.add(0, new ShowCardBrieflyEffect(tmpCard));
+                AbstractDungeon.topLevelEffects.add(new UpgradeShineEffect(tmpCard.target_x, tmpCard.target_y));
             }
         }, amount, c -> c.canUpgrade(), CardGroupType.DRAW_PILE, CardGroupType.DISCARD_PILE);
     }
